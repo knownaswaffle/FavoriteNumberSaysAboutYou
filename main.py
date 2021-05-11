59a3b12ab242f1225ede848c163afeeb2afdf25d
WaffleGodTTV <>
2021-05-11 15:41:13 +0000

    Added Quotes

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import time
 import os
 import sys
 import random
+from colorama import Fore, init
 
+
+which = input(Fore.GREEN + "Do you want to use the what your number says about you? (1) or the Quote Generator(2)")
 
 def slowprint(s):
     for c in s + '\n':
@@ -11,33 +14,36 @@ def slowprint(s):
         time.sleep(1. / 10)
 
 
-fee = open("quotes.txt", 'r').read().splitlines()
-n = input("What is your favorite number between one to nine: \n")
-
-fe = random.choice(fee)
-
-f = open("num.txt", 'a')
-f.write(f"{n}\n")
-f.close()
-
-youare = "Your number says that you are"
-
-if n == "1":
+if which == "1":  
+  n = input(Fore.GREEN + "What is your favorite number between one to nine: \n")
+  f = open("num.txt", 'a')
+  f.write(f"{n}\n")
+  f.close()
+  youare = "Your number says that you are"
+  if n == "1":
+    slowprint(f"{youare} a nice and intelligent person")
+    
+  if n == "2":
     slowprint(f"{youare} a nice and intelligent person.")
-if n == "2":
+  if n == "3":
     slowprint(f"{youare} a nice and intelligent person.")
-if n == "3":
+  if n == "4":
     slowprint(f"{youare} a nice and intelligent person.")
-if n == "4":
+  if n == "5":
     slowprint(f"{youare} a nice and intelligent person.")
-if n == "5":
-    slowprint(f"{youare} a nice and intelligent person.")
-if n == "6":
+  if n == "6":
     slowprint(f"{youare} a nice and intelligent person.")
 
-if n == "7":
+  if n == "7":
     slowprint(f"{youare} a nice and intelligent person.")
-if n == "8":
+  if n == "8":
     slowprint(f"{youare} a nice and intelligent person.")
-if n == "9":
+  if n == "9":
     slowprint(f"{youare} ")
+
+
+if which == "2":
+  fee = open("quotes.txt", 'r').read().splitlines()
+  fe = random.choice(fee)
+  print("Here's a quote for you!")
+  slowprint(f"{fe}")
